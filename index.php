a478921283511737ed5afb6ff916990c716ffce7
alexb60 <alexandrebertrand60@yahoo.fr>
2022-03-15 13:52:39 +0100

    ajout maquette bootstrap

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Groupe Project</title>
+
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/main.css">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+</head>
+
+<body>
+
+    <?php include('includes/_header.html'); ?>
+
+    <?php include('includes/_yves.html'); ?>
+    <?php include('includes/_rudy.html'); ?>
+    <?php include('includes/_alexandre.html'); ?>
+    <?php include('includes/_clement.html'); ?>
+
+    <?php include('includes/_footer.html'); ?>
+
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
